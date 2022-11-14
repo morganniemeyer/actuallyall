60acff1f28c48c3824227f4d3fc5b9c44b484d9e
Morgan Niemeyer <malineniemeyer@gmail.com>
2022-11-14 17:48:45 -0600

    passing games get all test

--- __tests__/games.test.js
+++ __tests__/games.test.js
@@ -10,7 +10,40 @@ describe('backend-express-template routes', () => {
   it('should GET list of games', async () => {
     const res = await request(app).get('/games');
     expect(res.status).toBe(200);
-    expect(res.body).toMatchInlineSnapshot();
+    expect(res.body).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "id": "1",
+          "name": "Azul",
+          "played": true,
+          "type": "tile-placement",
+        },
+        Object {
+          "id": "2",
+          "name": "Corinth",
+          "played": true,
+          "type": "roll-and-write",
+        },
+        Object {
+          "id": "3",
+          "name": "Flamecraft",
+          "played": false,
+          "type": "worker placement",
+        },
+        Object {
+          "id": "4",
+          "name": "Ticket To Ride",
+          "played": true,
+          "type": "route completion",
+        },
+        Object {
+          "id": "5",
+          "name": "Sagrada",
+          "played": true,
+          "type": "dice-placement",
+        },
+      ]
+    `);
   });
   afterAll(() => {
     pool.end();
