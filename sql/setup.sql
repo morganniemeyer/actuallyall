882c9b4e962069634ee50b1c1071bb6692486ed4
Morgan Niemeyer <malineniemeyer@gmail.com>
2022-11-14 11:49:59 -0600

    set up database

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,22 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS conventions;
+
+CREATE TABLE conventions (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR,
+    genre  VARCHAR,
+    season VARCHAR,
+    structure VARCHAR
+);
+
+INSERT INTO conventions (
+    name, genre, season, structure
+)
+VALUES
+('Twin Cities Con', 'multi', 'fall/winter', 'corporation'),
+('Anime Detour', 'anime', 'spring', 'non-profit'),
+('Anime Nebraskon', 'anime', 'fall', 'non-profit'),
+('C2E2', 'multi', 'multi', 'unknown'),
+('Ai-Kon', 'anime', 'summer', 'unknown');
+
