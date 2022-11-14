08493309d7360beab1d6a6ec06d13fb8c1ecc432
Morgan Niemeyer <malineniemeyer@gmail.com>
2022-11-14 12:30:11 -0600

    passing get all conventions test

--- __tests__/conventions.test.js
+++ __tests__/conventions.test.js
@@ -7,10 +7,48 @@ describe('conventions routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('should GET list of conventions', () => {
+  it('should GET list of conventions', async () => {
     const res = await request(app).get('/conventions');
     expect(res.status).toBe(200);
-    expect(res.body).toMatchInlineSnapshot();
+    expect(res.body).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "genre": "multi",
+          "id": "1",
+          "name": "Twin Cities Con",
+          "season": "fall/winter",
+          "structure": "corporation",
+        },
+        Object {
+          "genre": "anime",
+          "id": "2",
+          "name": "Anime Detour",
+          "season": "spring",
+          "structure": "non-profit",
+        },
+        Object {
+          "genre": "anime",
+          "id": "3",
+          "name": "Anime Nebraskon",
+          "season": "fall",
+          "structure": "non-profit",
+        },
+        Object {
+          "genre": "multi",
+          "id": "4",
+          "name": "C2E2",
+          "season": "multi",
+          "structure": "unknown",
+        },
+        Object {
+          "genre": "anime",
+          "id": "5",
+          "name": "Ai-Kon",
+          "season": "summer",
+          "structure": "unknown",
+        },
+      ]
+    `);
   });
   afterAll(() => {
     pool.end();
