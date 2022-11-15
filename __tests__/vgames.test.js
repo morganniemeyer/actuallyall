5698683885585da714c395bfefc2f947b0c73c62
Morgan Niemeyer <malineniemeyer@gmail.com>
2022-11-15 11:57:52 -0600

    passing get all for video games, add test for get one videogame

--- __tests__/vgames.test.js
+++ __tests__/vgames.test.js
@@ -10,6 +10,44 @@ describe('vgames routes', () => {
   it('should GET list of  video games', async () => {
     const res = await request(app).get('/vgames');
     expect(res.status).toBe(200);
+    expect(res.body).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "id": "1",
+          "name": "Harvest Moon",
+          "played": true,
+          "type": "farming",
+        },
+        Object {
+          "id": "2",
+          "name": "Animal Crossing",
+          "played": true,
+          "type": "real time sim",
+        },
+        Object {
+          "id": "3",
+          "name": "Katamari Damacy",
+          "played": true,
+          "type": "rolling",
+        },
+        Object {
+          "id": "4",
+          "name": "Sims",
+          "played": true,
+          "type": "life sim",
+        },
+        Object {
+          "id": "5",
+          "name": "Civilization 6",
+          "played": true,
+          "type": "strategy",
+        },
+      ]
+    `);
+  });
+  it('should GET vgames/1', async () => {
+    const res = await request(app).get('/vgames/1');
+    expect(res.status).toBe(200);
     expect(res.body).toMatchInlineSnapshot();
   });
   afterAll(() => {
